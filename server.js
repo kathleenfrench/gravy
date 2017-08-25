b528f443483fc11951d97f6774df034d2b2f7a8f
Kathleen French <kfrench09@gmail.com>
2017-08-25 15:48:49 -0400

    add application base html

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const es6Renderer = require('express-es6-template-engine');
 
+// es6 template engine
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+
+// config controllers to index
 app.use(require('./controllers'));
 
+// server port
 const port = process.env.PORT || 3000;
 
 app.listen(port);
