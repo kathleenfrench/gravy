8557b1394b1d0eec49fd869feef7dd9d74b06398
Kathleen French <kfrench09@gmail.com>
2017-08-25 18:30:42 -0400

    ind film detail ids included in views but no styling yet

--- controllers/movies.js
+++ controllers/movies.js
@@ -19,6 +19,16 @@ router
     .catch((err) => {
         return console.log(err);
     })
+})
+.get('/movies/:id', (req, res) => {
+  const movieURL = `http://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
+  axios.get(movieURL)
+  .then((api_res) => {
+    res.send(JSON.stringify(api_res));
+  }).catch((api_res) => {
+    console.log(api_res);
+  });
+
 });
 
 module.exports = router;
