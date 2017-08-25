7ea3102ebd238cc7f0aa9e7e986a08a6b19ee66b
Kathleen French <kfrench09@gmail.com>
2017-08-25 18:17:06 -0400

    clean up ui styling on main movie index page

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,9 +7,10 @@ router
     const popularMoviesURL = `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
     axios.get(popularMoviesURL)
         .then((api_res) => {
-            console.log(api_res);
-            
             res.render('application', {
+                locals: {
+                    movies: api_res.data
+                },
                 partials: {
                     yield: 'views/movies/index.html'
                 }
