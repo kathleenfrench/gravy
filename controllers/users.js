aea84b038e81e9cfc745c3fdffbad159c554f4b4
Kathleen French <kfrench09@gmail.com>
2017-08-28 17:02:12 -0400

    error messages on sign in form now config'd

--- controllers/users.js
+++ controllers/users.js
@@ -50,7 +50,8 @@ router
     res.render('application', {
         locals: {
             user: req.user,
-            csrfToken: req.csrfToken()
+            csrfToken: req.csrfToken(), 
+            errors: null
         },
         partials: {
             yield: 'views/users/sign-in.html'
@@ -60,17 +61,31 @@ router
 .post('/sign-in', csrfProtection, (req, res) => {
     models.User.findOne({ where: { email: req.body.email }})
     .then((user) => {
+        if (!user) throw new Error("Email or Password is incorrect.");
+
         return bcrypt.compare(req.body.password, user.password)
         .then((match) => {
             if(match){
                 const token = auth.generateToken(user);
                 res.cookie('gravy_token', token, { httpOnly: true, maxAge: 86400000 });
                 res.redirect('/');
+            } else {
+                throw new Error("Email or Password is incorrect.")
             }
         })
     })
     .catch((error) => {
-        return res.status(500);
+        return res.status(401);
+        return res.render('application', {
+            locals: {
+                user: req.user, 
+                csrfToken: req.csrfToken(), 
+                errors: error.message
+            },
+            partials: {
+                yield: 'views/users/sign-in.html'
+            }
+        })
     });
 });
 
