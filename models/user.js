9d32efe129d0f1351f1b1fea05330e2c3a8dca19
Kathleen French <kfrench09@gmail.com>
2017-08-28 16:18:43 -0400

    add validations to username, email, and passwords

--- models/user.js
+++ models/user.js
@@ -3,9 +3,35 @@ const bcrypt = require('bcryptjs');
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING, 
+      unique: true, 
+      validate: {
+        len: [4, 30],
+        isAlphanumeric: true, 
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING, 
+      unique: true, 
+      validate: {
+        isEmail: true, 
+        notEmpty: true
+      } 
+    },
+    // as is not allowing users to update info, if implemented later 
+    // then need to skip validation here and set up new custom validation
+    // and check for this.isNewRecord or use .save(fields: [...]) syntax
+    // to specify exactly which fields changed w/ only changed fields 
+    // having validates run against them
+    password: {
+      type: DataTypes.STRING, 
+      validate: {
+        notEmpty: true, 
+        len: [8, 50]
+      }      
+    }
   }, {
     classMethods: {
       associate: function(models) {
